57d99e079f2ae81c526f11149c082c85c7704610
meedaf11 <152024289+meedaf11@users.noreply.github.com>
2024-11-18 23:54:56 +0100

    semi Done

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ let categoriesContainer = document.getElementById("category_section");
 let booksContainer = document.getElementById("books_section");
 
 fetch("books.json").then(results => {
-    
+
     let myData = results.json();
     console.log(myData);
     return myData;
-}).then((myData) =>{
+}).then((myData) => {
 
     myData.forEach(data => {
 
         books.push(data);
-        
+
     });
 
     console.log(books);
@@ -30,87 +30,87 @@ console.log("wishList : ");
 console.log(wishListArray);
 
 
-function categoriesFunction(arrayBooks=[]){
+function categoriesFunction(arrayBooks = []) {
 
     let isCatAlredyAvailable = false;
 
-        
 
-    arrayBooks.forEach(book =>{
+
+    arrayBooks.forEach(book => {
 
         isCatAlredyAvailable = false;
 
-            for(let i = 0 ; i < categories.length ; i++){
+        for (let i = 0; i < categories.length; i++) {
 
-                if(categories[i] === book.categorie){
-                    isCatAlredyAvailable = true;
-                    break;
-
-                }
+            if (categories[i] === book.categorie) {
+                isCatAlredyAvailable = true;
+                break;
 
             }
 
-            if(!isCatAlredyAvailable){
-                categories.push(book.categorie);
+        }
 
-                categoriesContainer.innerHTML += `<div class="checkbox_containers">
+        if (!isCatAlredyAvailable) {
+            categories.push(book.categorie);
+
+            categoriesContainer.innerHTML += `<div class="checkbox_containers">
                 <input type="checkbox" value="${book.categorie}" id="box_${book.categorie}" name="categories" class="categoryBox">
                 <label for="box_${book.categorie}">${book.categorie}</label>
             </div>`;
 
             console.log(`box_${book.categorie}`);
-            }
+        }
 
     });
 
     console.log(categories);
 
-    
+
     let checkboxes = document.querySelectorAll(".categoryBox");
 
-    checkboxes.forEach(checkBox =>{
+    checkboxes.forEach(checkBox => {
 
         checkBox.addEventListener("click", () => {
 
             booksContainer.innerHTML = "";
 
             if (checkBox.checked) {
-                
+
                 checkboxes.forEach(otherCheckboxes => {
                     otherCheckboxes.checked = false;
                 });
-              
+
                 checkBox.checked = true;
 
-                arrayBooks.forEach(book =>{
+                arrayBooks.forEach(book => {
 
 
-                    if(book.categorie === checkBox.value){
+                    if (book.categorie === checkBox.value) {
 
                         let isInWL = false;
 
-                        for(let i = 0 ; i< wishListArray.length ; i++){
+                        for (let i = 0; i < wishListArray.length; i++) {
 
-                            if(book.title === wishListArray[i].title && book.author.fullName === wishListArray[i].author.fullName){
+                            if (book.title === wishListArray[i].title && book.author.fullName === wishListArray[i].author.fullName) {
                                 isInWL = true;
                                 break;
                             }
 
                         }
 
-                        if(isInWL){
+                        if (isInWL) {
                             booksContainer.innerHTML += `<div class="category_books">
 
             <img src="${book.cover}" alt="test book" class="book_cover">
             <h3 class="book_title">${book.title}</h3>
             <p class="book_authorName">${book.author.fullName}</p>
             <div class="content">
-                <img class="add_to_wl_icon" src="images/addedToWL.png" alt="add to wishlist">
+                <img class="add_to_wl_icon" src="images/addToWL.png" alt="add to wishlist">
             </div>
         </div>`;
-                        }else{
-                            
-                        booksContainer.innerHTML += `<div class="category_books">
+                        } else {
+
+                            booksContainer.innerHTML += `<div class="category_books">
 
                         <img src="${book.cover}" alt="test book" class="book_cover">
                         <h3 class="book_title">${book.title}</h3>
@@ -133,53 +133,53 @@ function categoriesFunction(arrayBooks=[]){
 
                 addToWishListImg.forEach(imageClick => {
 
-                imageClick.addEventListener('click',()=>{
-                    
-                    let isInWishList = false;
-                    let index;
-                    let title = imageClick.closest(".category_books").querySelector('.book_title').textContent;
-                    let authorName = imageClick.closest(".category_books").querySelector('.book_authorName').textContent;
+                    imageClick.addEventListener('click', () => {
 
-                    arrayBooks.forEach(book => {
+                        let isInWishList = false;
+                        let index;
+                        let title = imageClick.closest(".category_books").querySelector('.book_title').textContent;
+                        let authorName = imageClick.closest(".category_books").querySelector('.book_authorName').textContent;
 
-                        if(title === book.title && authorName === book.author.fullName){
+                        arrayBooks.forEach(book => {
 
-                            for(let i =0; i < wishListArray.length ; i++){
+                            if (title === book.title && authorName === book.author.fullName) {
 
-                                if(wishListArray[i].title === title && wishListArray[i].author.fullName === authorName){
-                                    isInWishList = true;
-                                    index=i;
-                                    break;
+                                for (let i = 0; i < wishListArray.length; i++) {
+
+                                    if (wishListArray[i].title === title && wishListArray[i].author.fullName === authorName) {
+                                        isInWishList = true;
+                                        index = i;
+                                        break;
+                                    }
+
                                 }
 
+                                if (!isInWishList) {
+                                    wishListArray.push(book);
+                                    imageClick.src = "images/addedToWL.png";
+                                    localStorage.setItem("books", JSON.stringify(wishListArray));
+                                } else {
+                                    imageClick.src = "images/addToWL.png";
+                                    wishListArray.splice(index, 1);
+                                    localStorage.setItem("books", JSON.stringify(wishListArray));
+                                }
+
+
+
                             }
 
-                            if(!isInWishList){
-                                wishListArray.push(book);
-                                imageClick.src = "images/addedToWL.png";
-                                localStorage.setItem("books", JSON.stringify(wishListArray));
-                            }else{
-                                imageClick.src = "images/addToWL.png";
-                                wishListArray.splice(index,1);
-                                localStorage.setItem("books", JSON.stringify(wishListArray));
-                            }
+                        });
 
-                            
+                        console.log(wishListArray);
 
-                        }
 
                     });
 
-                    console.log(wishListArray);
+                })
 
-
-            });
-
-        })
-                
 
             } else {
-                
+
                 checkBox.checked = false;
             }
 
@@ -187,7 +187,7 @@ function categoriesFunction(arrayBooks=[]){
 
     });
 
-    
+
 
 
 }
---
+++ wishlist.js
@@ -0,0 +1,57 @@
+let booksContainer = document.getElementById('book_container');
+let booksList = [];
+
+
+booksList = JSON.parse(localStorage.getItem("books")) || [];
+
+console.log(booksList);
+
+
+booksList.forEach(book => {
+
+            UpdateBooks(booksContainer, book.cover, book.title, book.description, book.author.fullName, book.releaseDate);
+    
+});
+
+let removeIcons = document.querySelectorAll('.wishlist_remove');
+
+removeIcons.forEach(icon =>{
+    icon.addEventListener('click',() => {
+
+        let wl_book_title = icon.closest(".wish_bookContainer").querySelector('.wl_book_title').textContent;
+        let wl_authot_name = icon.closest(".wish_bookContainer").querySelector('.wl_author_name').textContent;
+
+        for(let i =0 ; i<booksList.length ; i++){
+
+            if(wl_book_title == booksList[i].title && wl_authot_name == booksList[i].author.fullName){
+                booksList.splice(i , 1);
+                localStorage.setItem("books", JSON.stringify(booksList));
+                icon.closest(".wish_bookContainer").remove();
+            }
+
+        }
+
+    })
+});
+
+function UpdateBooks(Container, cover, title, description, authorName, releasedate){
+
+    Container.innerHTML +=`<div class="wish_bookContainer">
+
+                <img class="wl_book_cover" src="${cover}" alt="test Image">
+                <div class="book_content_details">
+
+                    <h3 class="wl_book_title">${title}</h3>
+                    <p>${description}</p>
+                    <p class="wl_author_name">${authorName}</p>
+                    <p>${releasedate}</p>
+
+
+                </div>
+                <div class="hoverContent">
+                    <img class="wishlist_remove" src="images/addedToWL.png" alt="add to wishlist">
+                </div>
+
+            </div>`;
+
+}
